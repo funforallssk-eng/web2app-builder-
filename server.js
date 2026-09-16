8dbfd37e98f762abbebf9641138aa24ccf330856
funforallssk-eng <funforallssk@gmail.com>
2026-09-16 12:53:16 +0530

    Remove unnecessary blank line in server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -70,4 +69,3 @@ app.get('*', (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('247 Software Running - No Cache'));
-
